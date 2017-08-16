176552f972c99ea18c5f38b989e8cb56cf963b69
Xertor <marsp@abv.bg>
2017-08-17 01:27:54 +0300

    BT77 Programmatic Sideways Friction

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,6 +3,25 @@
 #include "BattleTank.h"
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	// Calculate the slippage speed
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	// Work out the required acceleration this frame to correct
+	auto CorrectingAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	
+	// Calculate and apply sideways ( F = m a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectingForce = (TankRoot->GetMass() * CorrectingAcceleration) / 2; // Two tracks
+	TankRoot->AddForce(CorrectingForce);
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
